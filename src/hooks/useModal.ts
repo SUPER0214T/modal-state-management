63f25b1aebdd0064604169c7c132d5cda1bd9591
JunYoung <twinjyjh2@gmail.com>
2023-03-18 23:58:32 +0900

    navigate를  custom hook에서 가져오도록 하기 (근데 닫는 함수에서 이동까지 함께하는 것이 맞는 방식일까?)

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,20 +1,26 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ModalList } from '../components/Modal/ModalContainer';
 import { ModalSetterContext } from '../context/ModalProvider';
 
 function useModal() {
   const setter = useContext(ModalSetterContext);
+  const navigate = useNavigate();
 
   const setModalOpen = (type: ModalList) => {
     setter((prev) => [...prev, { type }]);
   };
 
-  const setModalClose = () => {
+  const setModalClose = (goTo?: string) => {
     setter((prev) => {
       const result = [...prev];
       result.pop();
       return result;
     });
+
+    if (goTo !== undefined) {
+      navigate(goTo);
+    }
   };
 
   return { setModalOpen, setModalClose };
